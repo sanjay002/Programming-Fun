0833b16724f5ae257d5e7c56258433010cf45c4e
sanjay002 <adhikarisanjay24@gmail.com>
2016-12-10 09:50:43 +0545

    Add files via upload

---
+++ circumference of c.cpp
@@ -0,0 +1,22 @@
+/*
+Student Name:Sanjay Adhikari
+Subject:Programming Fundamental
+Lab Sheet No:07
+Progam:WAP to enter marks of seven subjects
+Date:28 Nov,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+	float r,area;
+	printf("enter the radius of circle\n");
+	scanf("%f",&r);
+	
+	area = 3.1415*r*r;
+	printf("area of circumference of circle is  ::%f",area);
+	getch();
+	return 0;
+}
